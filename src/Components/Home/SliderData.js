d528f4d17b867ab6e7ac1673de392949294b448f
Morvin-Ian <morvinian@gmail.com>
2022-07-05 14:43:10 +0300

    responsive

--- src/Components/Home/SliderData.js
+++ src/Components/Home/SliderData.js
@@ -1,31 +1,22 @@
-import african from "../../Images/african1.jpg"
-import sisters from "../../Images/sisters.jpg"
-import kid from "../../Images/kid.jpg"
-import depression from '../../Images/depression.jpg'
-
-
-
-
-
 export const SliderData = [
     {
-        image:african,
+        image:"https://images.unsplash.com/photo-1568675392503-1cf97bb81c23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YWZyaWNhbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
         description:"African Traditions and Customs",
    
     },
 
   {
-        image:sisters,
+        image:"https://images.unsplash.com/photo-1611432580340-af48bd7549ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YmxhY2slMjB3b21lbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
         description:"Women Empowerment",
 
     },
     {
-        image:depression,
-        description:"Mental/Physical Health",
+        image:"https://images.unsplash.com/photo-1631883055788-5a5b15fae074?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGRlcHJlc3Npb258ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        description:"Mental Health/Depression",
     
     },
     {
-        image:kid,
+        image:"https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFzaGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
         description:"Classic Outfits (Kids/Grown-ups)",
     
     }
