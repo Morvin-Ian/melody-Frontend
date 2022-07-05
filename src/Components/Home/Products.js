89f045a2ea20db3299ea8a1225d169e5e1f52c14
Morvin-Ian <morvinian@gmail.com>
2022-07-05 11:50:26 +0300

    responsive

--- src/Components/Home/Products.js
+++ src/Components/Home/Products.js
@@ -16,11 +16,11 @@ const Products = ({products}) => {
                             <div className='ImageCont'>
                                 <img src={`https://melody-talk.herokuapp.com${product.image}`} alt=""hoodie/>
                                 <div className='description'>
-                                    <p>{product.product_name}-${product.price}</p>
+                                    <p id='desc'>{product.product_name}-${product.price}</p>
                                     {product.stock === 0 ?
-                                    <Button variant="contained" color="error"><Link style={{textDecoration:"none", color:"white"}} to={`/product/${product.id}`}>OUT OF STOCK</Link></Button> :
+                                    <Button id='btn' variant="contained" color="error"><Link style={{textDecoration:"none", color:"white"}} to={`/product/${product.id}`}>OUT OF STOCK</Link></Button> :
 
-                                    <Button variant="outlined"><Link style={{textDecoration:"none", color:"white"}} to={`/product/${product.id}`}>VIEW DETAILS</Link></Button>
+                                    <Button id='btn' variant="outlined"><Link style={{textDecoration:"none", color:"white"}} to={`/product/${product.id}`}>VIEW DETAILS</Link></Button>
                                     }
                                 </div> 
                             </div>
